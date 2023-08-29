9e445ca809e7b2f9e9aa06b1599aa2050a7ee863
Isha Paghdal <ishapaghdal@gmail.com>
2023-08-29 10:18:34 +0530

    logout

--- server/app.js
+++ server/app.js
@@ -1,6 +1,5 @@
 // app.js
 const express = require('express');
-// const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const gravatar = require('gravatar');
