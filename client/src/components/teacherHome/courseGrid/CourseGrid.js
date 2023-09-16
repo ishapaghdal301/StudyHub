d7aa26647c24c81cfa32ea02f1936e75d8730af7
Het Panchal <panchalhet18@gmail.com>
2023-09-16 14:35:00 +0530

    minor changes backend

--- client/src/components/teacherHome/courseGrid/CourseGrid.js
+++ client/src/components/teacherHome/courseGrid/CourseGrid.js
@@ -1,20 +1,46 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import "./coursegrid.css";
 import OneCourse from "./OneCourse";
 import './recommendations.css';
-const CourseGrid = ({ courses }) => {
+const CourseGrid = () => {
+    const [courses, setCourses] = useState([]);
+
+  useEffect(() => {
+    const instructor = localStorage.getItem("user");
+    async function fetchcourses() {
+      try {
+        const response = await fetch("http://localhost:5000/courses", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            instructor
+          }),
+        });
+
+        if (response.ok) {
+          const data = await response.json();
+          setCourses(data);
+          console.log(courses);
+        } else {
+          console.error("Failed to fetch courses");
+        }
+      } catch (error) {
+        console.error("Error fetching courses:", error);
+      }
+    }
+
+    fetchcourses();
+  }, []);
+
+
     console.log(courses);
 
     return (
         <div className="recommendationsDiv">
             <div className="recommendations">
-                {/* <h3>The world's largest selection of courses</h3>
-                <p>
-                    Choose from 130,000 online video courses with new additions published
-                    every month
-                </p> */}
                 <br></br>
-                {/* <br></br> */}
                 
                 <h2>Your Course</h2>
                 
--- client/src/components/teacherHome/teacherHome.js
+++ client/src/components/teacherHome/teacherHome.js
@@ -14,7 +14,7 @@ function TeacherHome() {
       <HeaderPrimary />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/addcourse" element={<AddCourse />} />
+        <Route path="/view-lessons" element={<AddCourse />} />
         <Route path="/updatecourse" element={<UpdateCourse />} />
       </Routes>
     </div>
@@ -22,43 +22,10 @@ function TeacherHome() {
 }
 
 function Home() {
-  const [courses, setCourses] = useState([]);
-
-  useEffect(() => {
-    const instructor = localStorage.getItem("user");
-    async function fetchcourses() {
-      try {
-        const response = await fetch("http://localhost:5000/courses", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            instructor
-          }),
-        });
-
-        if (response.ok) {
-          const data = await response.json();
-          setCourses(data);
-          console.log(courses);
-        } else {
-          console.error("Failed to fetch courses");
-        }
-      } catch (error) {
-        console.error("Error fetching courses:", error);
-      }
-    }
-
-    fetchcourses();
-  }, []);
 
   return (
     <>
-    <CourseGrid courses={courses} />
-      {/* <TeacherBody/> */}
-      {/* <TeacherUpdate/> */}
-      {/* <TeacherView/> */}
+    <CourseGrid/>
     </>
   );
 }
